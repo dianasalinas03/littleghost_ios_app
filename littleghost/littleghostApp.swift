f5b6ba2974b9c9d9978e20190e9978f38b43c47f
Diana Salinas <diana_salinas03@hotmail.com>
2026-05-30 02:42:12 -0600

    Initial Commit

---
+++ littleghost/littleghostApp.swift
@@ -0,0 +1,17 @@
+//
+//  littleghostApp.swift
+//  littleghost
+//
+//  Created by Diana⭐ on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct littleghostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
